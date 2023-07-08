4248743d080632a37e9c4086a346e1f2dc8171e2
RohanMagar11 <rmtechyt@gmail.com>
2023-07-08 11:11:39 +0530

    new function is add for teble file

--- table.py
+++ table.py
@@ -2,4 +2,5 @@
 
 for number in range(1,11):
     for count in range (1,11):
-        print (number, 'x', count, '=', number *count)
+        print (number, 'x', count, '=', number *count);
+print("magarohan")
